dd93384ecdd24e07930df697aa1409a1faf6345c
Cristian Camilo Rengifo Valencia <camilo3149@gmail.com>
2018-04-17 18:11:52 -0500

    CV
    add my resume

---
+++ Analisis/Hoja de vida/js/newjavascript.js
@@ -0,0 +1,5 @@
+var user = prompt('Enter the password to enter:');
+while(user=='' || user == null) {window.alert('Please, enter a password.');
+var user = prompt('Please, enter a password')}
+	document.write(time + ' ' + user);
+	window.alert('Welcome ' + user + '.')
