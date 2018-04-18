394147a9437374565a0fe76bbc33ebb0cba86fae
Cristian Camilo Rengifo Valencia <camilo3149@gmail.com>
2018-04-17 21:37:22 -0500

    module 7
    delivery of all exercises of module 7

---
+++ Analisis/Miriadax/modulo7/galeria.js
@@ -0,0 +1,26 @@
+var galeria = [
+   { persona:"Buddha - à¤¬à¥à¤¦à¥à¤§à¤¾",
+     frase:"En la confrontaciÃ³n entre el arrollo y la roca, el arrollo siempre ganarÃ¡, no por la fuerza, sino por la persistencia.",
+     foto:"http://www.imagexia.com/img/Cara-de-Buda.jpg"
+   },
+   { persona:"Khalil Gibran - Ø¬Ø¨Ø±Ø§Ù† Ø®Ù„ÙŠÙ„ Ø¬Ø¨Ø±Ø§Ù† Ø¨Ù† Ù…ÙŠØ®Ø§Ø¦Ù„ Ø¨Ù† Ø³Ø¹Ø¯",
+     frase:"El silencio del envidioso estÃ¡ lleno de ruidos.",
+     foto:"http://www.hannaharendtcenter.org/wp-content/uploads/2015/05/111.jpg"
+   },
+   { persona:"Confucio - å­”å­",
+     frase: "Todo tiene belleza pero no todo el mundo la puede ver.",
+     foto:"http://3.bp.blogspot.com/-VlsuMSoivLU/VeMlD-LymUI/AAAAAAABKPU/Q8PYwsFbqxg/s1600/confucio.jpeg"
+   },
+   { persona:"Lev NikolÃ¡ievich TolstÃ³i - Ð›ÐµÐ² ÐÐ¸ÐºÐ¾Ð»Ð°ÐµÐ²Ð¸Ñ‡ Ð¢Ð¾Ð»ÑÑ‚Ð¾Ð¹",
+     frase:"Mi felicidad consiste en que sÃ© apreciar lo que tengo y no deseo con exceso lo que no tengo.",
+     foto:"http://malba.s3-website-sa-east-1.amazonaws.com/wp-content/uploads/2014/09/tolstoi-05.jpg"
+   },
+   { persona:"PlatÃ³n - Î Î»Î¬Ï„Ï‰Î½",
+     frase:"El mÃ¡s importante y principal negocio pÃºblico es la buena educaciÃ³n de la juventud.",
+     foto:"https://s-media-cache-ak0.pinimg.com/236x/ee/c4/f3/eec4f3420f7024c58f1b44de233d8ecd.jpg"
+   },
+   { persona:"Henrik Ibsen - hÉ›nÉ¾Éªk ËˆjoËhÉ‘n ËˆÉªpsÉ™n",
+     frase:"Si dudas de ti mismo, estÃ¡s vencido de antemano.",
+     foto:"https://ebooks.adelaide.edu.au/i/ibsen/henrik/gosse/images/bust2.jpg"
+   }
+];
